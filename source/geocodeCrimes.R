50d11e38058dcd859023d16d8f64f951f8bcd3a3
Christopher Prener <chris.prener@slu.edu>
2018-09-15 19:53:31 -0500

    continue developing workflow

--- source/geocodeCrimes.R
+++ source/geocodeCrimes.R
@@ -1,7 +1,9 @@
 # executed *manually* as part of the buildCrime.Rmd notebook
 
-missingXY <- mutate_geocode(missingXY, location = fullAddress, output = "latlon", source = "google")
+part1Crimes_miss <- mutate_geocode(part1Crimes_miss, location = fullAddress, output = "latlon", source = "google")
 
-missingXY <- filter(missingXY, is.na(lon) == FALSE)
+part1Crimes_success <- filter(part1Crimes_miss, is.na(lon) == FALSE | ILEADSStreet != "UNKNOWN")
+part1Crimes_fail <- filter(part1Crimes_miss, is.na(lon) == TRUE | ILEADSStreet == "UNKNOWN")
 
-write_csv(missingXY, here("data", "raw", "stlmpd", "intermediate", "missingXY.csv"))
+write_csv(part1Crimes_success, here("data", "raw", "stlmpd", "intermediate", "missingXY_success.csv"))
+write_csv(part1Crimes_fail, here("data", "raw", "stlmpd", "intermediate", "missingXY_fail.csv"))
